a66c805049a133e77825e495d05d4724bbd0f478
Patrick Twohig <patrick@namazustudios.com>
2015-09-20 02:00:11 -0700

    Added boostrap method.

--- mina-guice/src/main/java/org/apache/mina/guice/MinaScopes.java
+++ mina-guice/src/main/java/org/apache/mina/guice/MinaScopes.java
@@ -1,10 +1,13 @@
 package org.apache.mina.guice;
 
+import org.apache.mina.core.future.ConnectFuture;
 import org.apache.mina.core.session.IoSession;
 
 import com.google.inject.Key;
 import com.google.inject.Provider;
 import com.google.inject.Scope;
+
+import java.util.concurrent.Callable;
 
 /**
  * A set of scopes for Mina.
@@ -21,6 +24,7 @@ public class MinaScopes {
 
 		@Override
 		public <T> Provider<T> scope(final Key<T> key, final Provider<T> unscoped) {
+            final IoSession
 			return new Provider<T>() {
 
 				@SuppressWarnings("unchecked")
@@ -50,5 +54,32 @@ public class MinaScopes {
 		}
 
 	};
+
+	/**
+	 * Given the {@link ConnectFuture}, this will wait for the connection to succeed, and then execute the
+     * given {@link Callable} immediately after a successful call.
+     *
+     * This is useful because you may want to bootstrap {@link IoSession} scope bound objects immediately
+     * upon connection, and this will enusre that the calling thread will ahve all the appropraite scoping
+     * properly configured.
+     *
+	 * @param connectFuture
+	 * @param callable
+	 * @param <T>
+	 * @return
+	 * @throws Exception
+	 */
+	public static <T> T bootstrap(final ConnectFuture connectFuture, final Callable<T> callable) throws Exception {
+
+        final IoSession ioSession = connectFuture.awaitUninterruptibly().getSession();
+
+        try {
+            IoSessionProvider.setSession(ioSession);
+            return callable.call();
+        } finally {
+            IoSessionProvider.freeSession(ioSession);
+        }
+
+    }
 
 }
